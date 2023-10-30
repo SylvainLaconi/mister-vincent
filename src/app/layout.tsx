048790780c748deee806cd666f9258f252456042
Sylvain Laconi <sylvainlaconi@MacBook-Air-de-Sylvain.local>
2023-10-30 11:53:45 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Mister Vincent - Home Organizer',
+  description: 'Besoin de place ? De vider une maison ? Vous déménagez ? Je suis là pour vous aider !',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
