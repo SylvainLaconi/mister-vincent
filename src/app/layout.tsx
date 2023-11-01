e40145ca54fc962cda028d34764d334f5f07dcd0
Sylvain Laconi <sylvainlaconi@MacBook-Air-de-Sylvain.local>
2023-11-01 18:39:50 +0100

    add Lexend_Deca font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Lexend_Deca } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const lexendDeca = Lexend_Deca({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Mister Vincent | Home Organizer & Coach en rangement à Lyon',
-  description: "Je vous accompagne pour optimiser, transformer et organiser vos espaces de vie et de travail. A l'écoute de vos contraintes et de vos besoins pour vous proposer des solutions adaptées à votre mode de vie.",
-}
+	title: 'Mister Vincent | Home Organizer & Coach en rangement à Lyon',
+	description:
+		"Je vous accompagne pour optimiser, transformer et organiser vos espaces de vie et de travail. A l'écoute de vos contraintes et de vos besoins pour vous proposer des solutions adaptées à votre mode de vie.",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={lexendDeca.className}>{children}</body>
+		</html>
+	);
 }
