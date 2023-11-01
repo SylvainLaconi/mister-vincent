905050e79385fc868980ea827a4b7d1f2e4e1b1b
Sylvain Laconi <sylvainlaconi@MacBook-Air-de-Sylvain.local>
2023-11-01 18:19:53 +0100

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
 
-      <div className="flex flex-col items-center justify-center"
+      <div className="flex flex-col items-center justify-center p-5"
             style={{
             backgroundImage: `url('/mister-vincent-hero.webp')`,
             height: "100vh",
@@ -12,7 +12,7 @@ export default function Home() {
             backgroundPosition: "center",
             backgroundSize: "cover",
       }}>
-        <h1 className='text-6xl font-semibold text-center text-[#c6d200]'>Mister Vincent</h1>
+        <h1 className='text-5xl font-semibold text-center text-[#c6d200]'>Mister Vincent</h1>
 
         <div className="flex place-items-center pt-10">
           <Image
@@ -24,8 +24,8 @@ export default function Home() {
           />
         </div>
         <p className='pt-10 text-center'>Mon site est en construction, mais je suis prêt à vous aider !</p> 
-        <p>Contactez-moi au <a href="tel:+33664758405" className='font-semibold'>+33 6 64 75 84 05</a> ou par email à <a href='mailto:vincent@mister-vincent.fr' target="_blank" className='font-semibold'>vincent@mister-vincent.fr</a></p>
-        <p>Mister Vincent - Home Organizer</p>
+        <p className='pt-5 text-center'>Contactez-moi au <a href="tel:+33664758405" className='font-semibold'>+33 6 64 75 84 05</a> ou par email à <a href='mailto:vincent@mister-vincent.fr' target="_blank" className='font-semibold'>vincent@mister-vincent.fr</a></p>
+        <p className='pt-5 text-center text-white'>Mister Vincent - Home Organizer</p>
       </div>
     </main>
   )
