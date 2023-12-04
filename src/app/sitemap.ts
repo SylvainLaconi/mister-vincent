1aa8a63e6f20bfcebda5c65a21a8bbac91205888
Sylvain Laconi <sylvainlaconi@MacBook-Air-de-Sylvain.local>
2023-12-04 09:30:55 +0100

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,37 +3,37 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.mister-vicent.fr/',
+      url: 'https://mister-vicent.fr/',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
     },
     {
-      url: 'https://www.mister-vicent.fr/home-organiser',
+      url: 'https://mister-vicent.fr/home-organiser',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.8,
     },
     {
-      url: 'https://www.mister-vicent.fr/office-organiser',
+      url: 'https://mister-vicent.fr/office-organiser',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.8,
     },
     {
-      url: 'https://www.mister-vicent.fr/death-cleaner',
+      url: 'https://mister-vicent.fr/death-cleaner',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.8,
     },
     {
-      url: 'https://www.mister-vicent.fr/cgu',
+      url: 'https://mister-vicent.fr/cgu',
       lastModified: new Date(),
       changeFrequency: 'never',
       priority: 0.1,
     },
     {
-      url: 'https://www.mister-vicent.fr/mentions-legales',
+      url: 'https://mister-vicent.fr/mentions-legales',
       lastModified: new Date(),
       changeFrequency: 'never',
       priority: 0.1,
