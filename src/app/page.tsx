17ec48cb024987b356564c599f201dc2ff58d1cc
Sylvain-xstudio <sylvain.laconi@x-studio.fr>
2025-08-19 08:43:04 +0200

    remove Pricing section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 			<ServiceSection />
 			{/* <AchievementSection /> */}
 			{/* <SocialProofSection /> */}
-			<PricingSection />
+			{/* <PricingSection /> */}
 			<ContactFormSection />
 			<FooterSection />
 		</main>
